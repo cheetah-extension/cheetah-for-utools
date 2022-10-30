403284f4979c39048c5e8415bc4c77d00fb9be6b
ronglecat <ronglecat@163.com>
2022-10-30 19:03:06 +0800

    feat: 接入 core 模块测试版

--- lib/common/core.ts
+++ lib/common/core.ts
@@ -1,14 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import fs from 'fs';
 import path from 'path';
-import crypto from 'crypto';
-import { getValue, notice } from './';
-import { ChildInfo, Project, Config, ResultItem } from './type';
-import { ignoreFolders } from './base';
-
-const HOME_PATH = process.env.HOME ?? process.env.APPDATA ?? '';
-
-const ignoreFoldersReg = new RegExp(ignoreFolders.join('|'), 'i');
+import { getValue, setValue, notice, errorHandle } from './utils';
+import {
+  HOME_PATH,
+  Project,
+  ResultItem,
+  init,
+  PlatformType,
+  clearCache,
+} from 'cheetah-core';
+import { platform } from './constant';
+import { query as linkQuery } from 'windows-shortcuts';
 
 // 缓存路径
 const cachePath = path.join(
@@ -19,263 +20,69 @@ const cachePath = path.join(
   'config.json'
 );
 
-// 获取文件的 md5 值
-export function getMd5(content: string) {
-  const hash = crypto.createHash('md5');
-  hash.update(content, 'utf8');
-  return hash.digest('hex');
-}
-
-// 写入缓存
-export async function writeCache(newCache: Project[]): Promise<void> {
-  try {
-    const { editor } = await readCache();
-    const newEditorList = combinedEditorList(editor, newCache);
-    const newConfig = { editor: newEditorList, cache: newCache };
-    const historyString = JSON.stringify(newConfig, null, 2);
-    fs.mkdirSync(path.dirname(cachePath), { recursive: true });
-    fs.writeFileSync(cachePath, historyString);
-  } catch (error: any) {
-    console.log(error.message);
-  }
-}
-
-// 合并编辑器
-function combinedEditorList(
-  editor: { [key: string]: string },
-  cache: Project[]
-) {
-  const newEditor = { ...editor };
-  const currentEditor = Object.keys(newEditor);
-  cache.forEach(({ type }: Project) => {
-    if (!currentEditor.includes(type)) {
-      newEditor[type] = '';
+export function getAllDefaultApp() {
+  return (
+    getValue('defaultApp') || {
+      open: '',
+      git_gui_open: '',
+      terminal_open: '',
     }
-  });
-  return newEditor;
-}
-
-// 更新缓存时合并项目点击数
-async function combinedCache(newCache: Project[]): Promise<Project[]> {
-  const { cache } = await readCache();
-  // 筛选有点击记录的项目
-  const needMergeList = {} as { [key: string]: Project };
-  cache
-    .filter((item: Project) => item.hits > 0 || item.idePath)
-    .forEach((item: Project) => {
-      needMergeList[item.path] = item;
-    });
-  // 合并点击数
-  newCache.forEach((item: Project) => {
-    const cacheItem = needMergeList[item.path] ?? {};
-    const { hits = 0, idePath = '' } = cacheItem;
-    item.hits = item.hits > hits ? item.hits : hits;
-    item.idePath = idePath;
-  });
-  return newCache;
-}
-
-// 读取文件内容
-function readFile(filePath: string): string {
-  return fs.readFileSync(filePath).toString();
-}
-
-// 读取缓存
-export function readCache(): Config {
-  try {
-    const history = readFile(cachePath);
-    return JSON.parse(history) ?? { editor: {}, cache: [] };
-  } catch (error: any) {
-    if (error.message === 'no such file or directory') {
-      writeCache([]);
-      return { editor: {}, cache: [] };
-    }
-    return { editor: {}, cache: [] };
-  }
-}
-
-// 查找博客目录
-export function findProject(dirPath: string): Project[] {
-  const result: Project[] = [];
-  const currentChildren: ChildInfo[] = [];
-  if (ignoreFoldersReg.test(dirPath)) {
-    return result;
-  }
-  let dirIter = [];
-  try {
-    dirIter = fs.readdirSync(dirPath);
-  } catch (error) {
-    return result;
-  }
-
-  for (const item of dirIter) {
-    const itemPath = path.join(dirPath, item);
-    let isDir = false;
-    try {
-      isDir = fs.statSync(itemPath).isDirectory();
-    } catch (error: any) {
-      //
-    }
-    currentChildren.push({
-      name: item,
-      isDir,
-      path: itemPath,
-    });
-  }
-
-  const isGitProject = currentChildren.some(
-    ({ name }: { name: string }) => name === '.git'
   );
-
-  const hasSubmodules = currentChildren.some(
-    ({ name }: { name: string }) => name === '.gitmodules'
-  );
-
-  if (isGitProject) {
-    result.push({
-      name: path.basename(dirPath),
-      path: dirPath,
-      type: projectTypeParse(currentChildren),
-      hits: 0,
-      idePath: '',
-    });
-  }
-
-  let nextLevelDir: ChildInfo[] = [];
-  if (!isGitProject) {
-    nextLevelDir = currentChildren.filter(
-      ({ isDir }: { isDir: boolean }) => isDir
-    );
-  }
-  if (isGitProject && hasSubmodules) {
-    nextLevelDir = findSubmodules(path.join(dirPath, '.gitmodules'));
-  }
-
-  for (let i = 0; i < nextLevelDir.length; i += 1) {
-    const dir = nextLevelDir[i];
-    result.push(...findProject(path.join(dirPath, dir.name)));
-  }
-  return result;
 }
 
-// 查找项目内的 submodule
-export function findSubmodules(filePath: string): ChildInfo[] {
-  const fileContent = readFile(filePath);
-  const matchModules = fileContent.match(/(?<=path = )([\S]*)(?=\n)/g) ?? [];
-  return matchModules.map((module) => {
-    return {
-      name: module,
-      isDir: true,
-      path: path.join(path.dirname(filePath), module),
-    };
+/**
+ * @description: 快捷方式解析，暂时用不上
+ * @param {string} path
+ * @return {*}
+ */
+export function getAppRealPath(path: string): Promise<string> {
+  return new Promise<string>((resolve) => {
+    if (platform.isMac && !platform.isLinux && !path.endsWith('.lnk')) {
+      resolve(path);
+    }
+    if (platform.isWin) {
+      linkQuery(path, (error, details) => {
+        if (error || !details) {
+          utools.showNotification('获取应用路径失败');
+          resolve('');
+          return;
+        }
+        resolve(details?.target ?? '');
+      });
+    }
+  });
+}
+
+/**
+ * @description: 设置默认应用
+ * @param {string} type 要设置的类型
+ * @param {string} appPath 要设置的应用路径
+ * @return {*}
+ */
+export function setDefaultApp(type: string, appPath: string): void {
+  const defaultApp = getAllDefaultApp();
+  defaultApp[type] = appPath;
+  setValue('defaultApp', defaultApp);
+}
+
+export function initCore() {
+  const workspaces = getValue('workspaces') || [];
+
+  let platformType: PlatformType = PlatformType.MAC;
+  if (utools.isWindows()) {
+    platformType = PlatformType.WINDOWS;
+  }
+  if (utools.isLinux()) {
+    platformType = PlatformType.LINUX;
+  }
+
+  init({
+    cachePath,
+    workspaces: workspaces.join(','),
+    platformType,
   });
 }
 
-// 判断项目下的文件列表是否包含需要搜索的文件列表
-function findFileFromProject(
-  allFile: ChildInfo[],
-  fileNames: string[]
-): boolean {
-  const reg = new RegExp(`^(${fileNames.join('|')})$`, 'i');
-  const findFileList = allFile.filter(({ name }: { name: string }) =>
-    reg.test(name)
-  );
-
-  return findFileList.length === fileNames.length;
-}
-
-function findDependFromPackage(
-  allDependList: string[],
-  dependList: string[]
-): boolean {
-  const reg = new RegExp(`^(${dependList.join('|')})$`, 'i');
-  const findDependList = allDependList.filter((item: string) => reg.test(item));
-
-  return findDependList.length >= dependList.length;
-}
-
-function getDependList(allFile: ChildInfo[]): string[] {
-  const packageJsonFilePath =
-    allFile.find(({ name }) => name === 'package.json')?.path ?? '';
-  if (!packageJsonFilePath) {
-    return [];
-  }
-  const { dependencies = [], devDependencies = [] } = JSON.parse(
-    readFile(packageJsonFilePath)
-  );
-  const dependList = { ...dependencies, ...devDependencies };
-  return Object.keys(dependList);
-}
-
-// 解析项目类型
-function projectTypeParse(children: ChildInfo[]): string {
-  if (findFileFromProject(children, ['cargo.toml'])) {
-    return 'rust';
-  }
-  if (findFileFromProject(children, ['pubspec.yaml'])) {
-    return 'dart';
-  }
-  if (findFileFromProject(children, ['.*.xcodeproj'])) {
-    return 'applescript';
-  }
-  if (findFileFromProject(children, ['app', 'gradle'])) {
-    return 'android';
-  }
-  // js 项目还可以细分
-  if (findFileFromProject(children, ['package.json'])) {
-    if (findFileFromProject(children, ['nuxt.config.js'])) {
-      return 'nuxt';
-    }
-    if (findFileFromProject(children, ['vue.config.js'])) {
-      return 'vue';
-    }
-    if (findFileFromProject(children, ['.vscodeignore'])) {
-      return 'vscode';
-    }
-
-    const isTS = findFileFromProject(children, ['tsconfig.json']);
-    const dependList = getDependList(children);
-
-    if (findDependFromPackage(dependList, ['react'])) {
-      return isTS ? 'react_ts' : 'react';
-    }
-
-    if (findDependFromPackage(dependList, ['hexo'])) {
-      return 'hexo';
-    }
-
-    return isTS ? 'typescript' : 'javascript';
-  }
-  return 'unknown';
-}
-
-// 过滤项目
-export function filterProject(
-  projectList: Project[],
-  keyword: string
-): Project[] {
-  const result = projectList.filter(({ name }: { name: string }) => {
-    const reg = new RegExp(keyword, 'i');
-    return reg.test(name);
-  });
-
-  // 排序规则：项目名称以关键词开头的权重最高，剩余的以点击量降序排序
-  const startMatch: Project[] = [];
-  const otherMatch: Project[] = [];
-  result.forEach((item) => {
-    if (item.name.startsWith(keyword)) {
-      startMatch.push(item);
-    } else {
-      otherMatch.push(item);
-    }
-  });
-
-  return [
-    ...startMatch.sort((a: Project, b: Project) => b.hits - a.hits),
-    ...otherMatch.sort((a: Project, b: Project) => b.hits - a.hits),
-  ];
-}
-// 输出待选列表给 Alfred
 export function output(projectList: Project[]): ResultItem[] {
   const result = projectList.map(
     ({
@@ -304,48 +111,12 @@ export function output(projectList: Project[]): ResultItem[] {
   return result;
 }
 
-// 在多个工作目录下搜索项目，工作目录以英文逗号分隔
-async function batchFindProject() {
-  const workspaces = getValue('workspaces') || [];
-  if (!workspaces.length) {
-    notice('请先配置工作目录');
-    return [];
-  }
-  const projectList: Project[] = [];
-  for (let i = 0; i < workspaces.length; i += 1) {
-    const dirPath = workspaces[i];
-    const children = await findProject(dirPath);
-    projectList.push(...children);
-  }
-  return projectList;
-}
-
-// 从缓存中过滤
-export async function filterWithCache(keyword: string): Promise<ResultItem[]> {
-  const { cache } = await readCache();
-  return output(filterProject(cache, keyword));
-}
-
-// 从搜索结果中过滤
-export async function filterWithSearchResult(
-  keyword: string
-): Promise<ResultItem[]> {
-  const projectList: Project[] = await batchFindProject();
-  writeCache(await combinedCache(projectList));
-  return filterWithCache(keyword);
-}
-
-// 清除缓存
-export function clearCache() {
+export async function onClearCache(): Promise<void> {
   try {
-    fs.unlinkSync(cachePath);
+    initCore();
+    await clearCache();
     notice('缓存清除成功');
-  } catch (error) {
-    notice('缓存清除失败');
+  } catch (error: any) {
+    errorHandle(error);
   }
 }
-
-export default {
-  filterWithCache,
-  filterWithSearchResult,
-};
